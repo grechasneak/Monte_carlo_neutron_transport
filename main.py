232fb3c623aca905b751e3b1efa5aac38b77b808
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-04-17 13:53:46 -0700

    Fixed cell tracking

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
-from __future__ import division 
-import random 
+from __future__ import division
+import random
 import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-
-
-class Tally():
-	def __init__(self):
-		self.thermal = 0
-		self.fast = 0 
-		self.current = 0
 
 class Neutron():
 	def __init__(self, grid, data):
@@ -25,120 +17,117 @@ class Neutron():
 		self.df = grid
 		self.xs = data
 		
+	def update_cell(self):
+		self.cell = int(self.location/spacing)
+		
 	def scatter(self):
 		self.direction = 2 * random.random() - 1
-		
+
 	def crossection(self, material, interaction):
 		return self.xs.ix[material, interaction]
 		
-	def update_cell(self):
-		self.cell = int(self.location/spacing)
-		if self.cell < 0:
-			self.cell = 0
-		if self.cell > 128:
-			self.cell = 128
-			
-		
-	def interaction(self):		
+
+	def distance(self, material):
+		if self.group == 1:
+			return abs(np.log(random.random())/self.crossection(material, 'sigma_t1'))
+		else:
+			return abs(np.log(random.random())/self.crossection(material, 'sigma_t2'))
+			 
+
+	def interaction(self):
 		while True:
 			self.update_cell()
-			
+
 			material = self.df.ix[self.cell, 1]
 
 			sigma_t1 = self.crossection(material, 0)
 			sigma_s11 = self.crossection(material, 1)
 			sigma_t2 = self.crossection(material, 3)
 			sigma_s22 = self.crossection(material, 4)
+
+			cell, track = self.move()
 			
-			
-			cell, track = self.move(self.cell)
 			
 			if self.group == 1:
 				if random.random()  <= (sigma_s11/sigma_t1): #is it within group?
 					yield ((cell, track), self.group)
-					
-				# down scatter	
+
+				# down scatter
 				else:
 					self.group = 2
 					yield ((cell, track), self.group)
-	
+
 			if self.group == 2:
 				if random.random() <= (sigma_s22/sigma_t2): #does it scatter or fission?
 					# scatter
 					yield ((cell, track), self.group)
-					
+
 				# fission
 				else:
 					#print('fission')
 					yield ((cell, track), self.group)
 					break
-					
-					
-	def distance(self, material):
-		if self.group == 1:
-			s = abs(np.log(random.random())/self.crossection(material, 'sigma_t1'))
-		if self.group == 2:
-			s = abs(np.log(random.random())/self.crossection(material, 'sigma_t2'))
-			return s
+
+
+
+
+	def move(self):
+		material = self.df.ix[self.cell, 1]
+		s = self.distance(material)
+		print(self.location)
 		
-	def move(self, cell):
-		while True:
+		right_cell_pos = self.cell * spacing + spacing
+		left_cell_pos = self.cell * spacing
 			
-			material = self.df.ix[cell, 1]
-			s = self.distance(material)
-			print(self.location)
-			
-			if self.direction > 0:
-				possible_location = self.direction * s + self.location
+
+		if self.direction > 0:
+			possible_location = self.direction * s + self.location
 				
-				if possible_location > 20:
-					distance = 20 - self.location
-					self.location = 19.999
-					self.direction = -self.direction
-					return self.cell, distance
+				
+			if possible_location > right_cell_pos: 
+				delta_x = right_cell_pos - self.location
+				traveled = delta_x / self.direction
+				self.location = right_cell_pos + 0.0001
 					
-				elif possible_location > (self.cell * spacing): # is s greater than the edge of the cell?
-					distance = (self.cell * spacing) - self.location
-					self.location = self.cell * spacing + 0.001 #if so move to the boundary resample distance into new cell
-					return self.cell, distance
-
-
-
-
+				if self.location > 20.0:
+					self.direction = - self.direction
+					self.location = 19.9999
+					
+				self.update_cell()	
+				return self.cell, traveled
+					
 				#if it is within the cell
-				else:
-					self.location = possible_location 
-					self.scatter()
-					return self.cell, s
-
+			else:
+				self.location = possible_location
+				self.scatter()
+				return self.cell, s
 
 
 		# if is traveling in the negative direction
-			if self.direction < 0:
-				possible_location = self.location + (self.direction * s)
+		if self.direction < 0:
+			possible_location = self.location + (self.direction * s)
 				
-				if possible_location < 0:
-					distance = self.location
-					self.location = 0.001
-					self.direction = -self.direction
-					return self.cell, distance
+				
+			if possible_location < left_cell_pos:
+				delta_x = self.location - left_cell_pos
+				traveled = abs(delta_x/self.direction)
+				self.location = left_cell_pos - 0.0001
 					
-				elif possible_location < (self.cell * spacing - spacing):
-					distance = self.location - (self.cell * spacing - spacing)
-					self.location = (self.cell * spacing) - spacing - 0.001
-					return self.cell, distance
+				if self.location < 0.0:
+					self.direction = - self.direction
+					self.location = 0.0001
+					
+				self.update_cell()	
+				return self.cell, traveled
+					
+			#still within the cell
+			else:
+				self.location = possible_location
+				self.scatter()
+				return self.cell, s
 
 
 
-				
-				#still within the cell
-				else:
-					self.location = possible_location 
-					self.scatter()
-					return self.cell, s
-
-
-			
 global spacing
 spacing = 0.15625
 
@@ -156,60 +145,25 @@ weight = 1
 
 grid = pd.read_csv('grid.csv')
 xs = pd.read_csv('XS.csv', index_col = 'material')
-		
+
 n = Neutron(grid, xs)
 
 flux_1 = np.zeros(129)
 flux_2 = np.zeros(129)
+n = Neutron(grid, xs)
+# for i in range(generations):
 
-for i in range(generations):
+	# for i in range(neutrons):
+			# n = Neutron(grid, xs)
+			# for track_data in n.interaction():
 
-	for i in range(neutrons):
-			n = Neutron(grid, xs)
-			for track_data in n.interaction():
-			
-				cell = track_data[0][0]
-				track = track_data[0][1]
-				group = track_data[1]
-				
-				if group == 1:
-					flux_1[cell] = flux_1[cell] + (track * weight) / (neutrons * spacing)
-				if group == 2:
-					flux_2[cell] = flux_2[cell] + (track * weight) / (neutrons * spacing)
-					
-				print(track)
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+				# cell = track_data[0][0]
+				# track = track_data[0][1]
+				# group = track_data[1]
+
+				# if group == 1:
+					# flux_1[cell] = flux_1[cell] + (track * weight) / (neutrons * spacing)
+				# if group == 2:
+					# flux_2[cell] = flux_2[cell] + (track * weight) / (neutrons * spacing)
+
+				# #print(track)
